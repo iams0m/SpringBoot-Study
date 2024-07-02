4462886a12b1424488000947c91073b515a87a63
iams0m <n980805@gmail.com>
2024-07-03 00:01:18 +0900

    Refactor: 댓글 조회 로직 중 for문 -> stream 문법으로 변경

--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -20,18 +21,8 @@ public class CommentService {
     private ArticleRepository articleRepository;
 
     public List<CommentDto> comments(Long articleId) {
-        // 1. 댓글 조회
-        List<Comment> comments = commentRepository.findByArticleId(articleId);
-
-        // 2. 엔티티 -> DTO 변환
-        List<CommentDto> dtos = new ArrayList<CommentDto>();
-        for (int i = 0; i < comments.size(); i++) {
-            Comment c = comments.get(i);
-            CommentDto dto = CommentDto.createCommentDto(c);
-            dtos.add(dto);
-        }
-
-        // 3. 결과 반환
-        return dtos;
+        return commentRepository.findByArticleId(articleId)
+                .stream().map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());
     }
 }
