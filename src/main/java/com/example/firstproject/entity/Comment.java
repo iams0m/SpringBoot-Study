08a09e516aef1eea87d6abcc54454a1f08d4390b
iams0m <n980805@gmail.com>
2024-07-04 14:20:49 +0900

    Feat: 댓글 생성 로직 추가

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,4 +27,22 @@ public class Comment {
 
     @Column
     private String body;
+
+    public static Comment createComment(CommentDto dto, Article article) {
+        
+        // 예외 발생
+        if (dto.getId() != null) {
+            throw new IllegalArgumentException("댓글 생성 실패! 댓글의 id가 이미 존재합니다.");
+        }
+        if (dto.getArticleId() != article.getId()) {
+            throw new IllegalArgumentException("댓글 생성 실패! 게시글의 id를 잘못 요청하고 있습니다.");
+        }
+        
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody());
+    }
 }
--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,13 +1,14 @@
 package com.example.firstproject.service;
 
 import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Comment;
 import com.example.firstproject.repository.ArticleRepository;
 import com.example.firstproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,5 +25,22 @@ public class CommentService {
         return commentRepository.findByArticleId(articleId)
                 .stream().map(comment -> CommentDto.createCommentDto(comment))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패! " +
+                "대상 게시글이 없습니다."));
+
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+
+        // 3. 댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+
+        // 4. DTO로 변환해 반환
+        return CommentDto.createCommentDto(created);
     }
 }
